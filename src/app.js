6cff9c098d9528f82fd9dbd9752834f75fdaca95
Gergely Gubcsi <gergo10@gmail.com>
2018-09-08 18:49:22 +0200

    fancy html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("pages"));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
